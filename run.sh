b1f5d906a0896e1b98f6b6b6c93816b105546175
TangXuanzhao <xuanzhaotang@gmail.com>
2018-07-28 17:51:35 +0800

    fix misspell

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ docker-compose down
 docker pull txzdream/go-online-service_end:dev
 docker pull txzdream/go-online-docker_end:dev
 docker pull txzdream/go-online-front_end:dev
-codker pull mysql:5.6
+dodker pull mysql:5.6
 docker pull redis
 docker pull marian/rebrow
 docker pull phpmyadmin/phpmyadmin
