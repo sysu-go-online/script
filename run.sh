c15f45a35352fa757eb260ffb89a4ec841f6136d
TangXuanzhao <xuanzhaotang@gmail.com>
2018-10-11 08:43:30 +0800

    rm redis client

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ docker pull txzdream/go-online-docker_end:dev
 docker pull txzdream/go-online-front_end:dev
 docker pull mysql:5.6
 docker pull redis
-docker pull marian/rebrow
 docker pull phpmyadmin/phpmyadmin
 docker pull consul
 docker pull txzdream/go-online-user-service-proxy:dev
