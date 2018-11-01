8712d065fbbde2ba7e9921b866a3e6c7c707eb34
TangXuanzhao <xuanzhaotang@gmail.com>
2018-11-01 09:50:26 +0800

    finish nginx debug

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 docker-compose down
-docker pull txzdream/go-online-service_end:dev
 docker pull txzdream/go-online-docker_end:dev
 docker pull txzdream/go-online-front_end:dev
+docker pull txzdream/go-online-user_service:dev
+docker pull txzdream/go-online-project_service:dev
+docker pull txzdream/go-online-file_service:dev
+docker pull txzdream/go-online-ws_service:dev
+docker pull txzdream/go-online-static_end:dev
+docker pull txzdream/go-online-auth_service:dev
 docker pull mysql:5.6
 docker pull redis
 docker pull phpmyadmin/phpmyadmin
